a13dd35004db4fbd75a2e21a58f62078b33f98b0
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-01-19 23:24:26 +0100

    don't lose the streak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// don't lose the streak
+
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
 import EventDetailPage, {
