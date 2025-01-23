7d8788523a987867b92505aa71853398a74b442f
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-01-23 21:55:51 +0100

    Fixed minor bugs

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,13 +1,21 @@
-import { Form, Link, useSearchParams, useActionData,  } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 // streak
 function AuthForm() {
   //we can allso acces to a function that allows to change the searchParams (setSearchParams)
+  const navigation = useNavigation("");
   const [searchParams] = useSearchParams();
-  const isLogin = searchParams.get("mode") === "login";
+  const mode = searchParams.get("mode") || "login"
+  const isLogin = mode === "login";
   const data = useActionData();
-
+  const isSubmitting = navigation.state === "submitting";
   return (
     <>
       <Form method="post" className={classes.form}>
@@ -29,10 +37,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? "singup" : "login"}`}>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting" : "Save"}
+          </button>
         </div>
       </Form>
     </>
