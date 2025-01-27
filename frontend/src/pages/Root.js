a662cf10a325e3c3f226113746838ee5ef51df3e
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-01-28 00:08:08 +0100

    Finished Project

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,8 @@
-import {
-  Outlet,
-  useLoaderData,
-  useSubmit,
-} from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
@@ -15,9 +12,14 @@ function RootLayout() {
     if (!token) {
       return;
     }
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+    const tokenDuration = getTokenDuration();
     setTimeout(() => {
       submit(null, { action: "/logout", method: "post" });
-    }, 1 * 60 * 60 * 1000);
+    }, tokenDuration);
   }, [submit, token]);
   return (
     <>
